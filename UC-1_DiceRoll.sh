060ca51a262cc41555c0623ce128fd25e25d2126
Manul <manuljain16@gmail.com>
2020-10-19 11:33:15 +0530

    2 dice roll

---
+++ UC-1_DiceRoll.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+count=0
+sum=0
+
+while true
+do
+    r1=$((1+RANDOM%6))
+    r2=$((1+RANDOM%6))
+    r3=$(($r1+$r2))
+
+    if [[ $r3 -ne 12 ]]
+    then
+        count=$(($count+1))
+        sum=$(($sum+$r3))
+    else
+        break
+
+    fi
+done
+echo "number of times dice roll : $count"
+echo "sum is : $sum"
